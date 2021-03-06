11777c58b6750d61a4d56769d5f6c71dad6eafc7
Saahil Jaffer <saahil.jaffer@uwaterloo.ca>
2021-03-06 18:41:48 -0500

    maps

--- frontend/src/pages/Route.js
+++ frontend/src/pages/Route.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link, Button, Container, Box } from "@material-ui/core";
+import { Redirect } from "react-router-dom";
 
 export function DriverRoute({ match }) {
   const { id } = match.params;
   const [data, setData] = useState(null);
   const [index, setIndex] = useState(0);
-  const [delivery, setDelivery] = useState(null);
+  const [link, setLink] = useState(null);
   useEffect(() => {
     var fetchStr = "/api/routes/" + id;
     fetch(fetchStr, {
@@ -15,23 +16,51 @@ export function DriverRoute({ match }) {
       .then(setData);
   }, []);
 
+  useEffect(() => {
+    if (data) {
+      setLink(
+        "https://www.google.com/maps/dir/?api=1&destination=" +
+          data.deliveries[index].number +
+          "+" +
+          data.deliveries[index].street +
+          "+" +
+          data.deliveries[index].postalCode
+      );
+    }
+  }, [index, data]);
+
   if (data) {
     return (
       <Container maxWidth="md">
         <div style={{ textAlign: "left" }}>
-          <Link href="https://google.ca">
-            Address: {data.deliveries[index].number}{" "}
-            {data.deliveries[index].street} {data.deliveries[index].postalCode}
-          </Link>
+          Address: {data.deliveries[index].number}{" "}
+          {data.deliveries[index].street} {data.deliveries[index].postalCode}
           <p>Apartment Number: {data.deliveries[index].apartment}</p>
           <p>Phone Number: {data.deliveries[index].phone}</p>
           <p>Portions: {data.deliveries[index].portions}</p>
-
           <Box m={1}>
-            <Button variant="contained">Previous</Button>
+            <Link href={link} key="signup" style={{ textDecoration: "none" }}>
+              {/* <Button variant="contained">Open Maps</Button> */}
+              Open Maps
+            </Link>
           </Box>
           <Box m={1}>
-            <Button variant="contained">Next</Button>
+            <Button
+              variant="contained"
+              disabled={index === 0}
+              onClick={() => setIndex(index - 1)}
+            >
+              Previous
+            </Button>
+          </Box>
+          <Box m={1}>
+            <Button
+              variant="contained"
+              disabled={index === data.deliveries.length - 1}
+              onClick={() => setIndex(index + 1)}
+            >
+              Next
+            </Button>
           </Box>
         </div>
       </Container>
